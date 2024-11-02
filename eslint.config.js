f266d86763f2c83c2b849cc4d014625d01d48ed8
Egor Bondar <egorbondar@MacBook-Air-Egor.local>
2024-11-02 18:07:35 +1000

    feat: setup commit
    Signed-off-by: Egor Bondar <egorbondar@MacBook-Air-Egor.local>
    
    feat: deleted husky
    
    Signed-off-by: Egor Bondar <egorbondar@MacBook-Air-Egor.local>
    
    fix: delete vscode
    
    Signed-off-by: Egor Bondar <egorbondar@MacBook-Air-Egor.local>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  ignorePatterns: ["projects/**/*"],
   overrides: [
     {
       files: ["*.ts"],
@@ -7,21 +8,43 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates",
         "plugin:prettier/recommended"
       ],
-    },
-    {
-      files: ["*.html"],
-      extends: ["plugin:@angular-eslint/template/recommended"],
-      rules: {},
-    },
-    {
-      files: ["*.html"],
-      excludedFiles: ["*inline-template-*.component.html"],
-      extends: ["plugin:prettier/recommended"],
       rules: {
-        "prettier/prettier": ["error", { parser: "angular" }]
-      },
+        "@angular-eslint/directive-selector": [
+          "error",
+          {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase"
+          }
+        ],
+        "@angular-eslint/component-selector": [
+          "error",
+          {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case"
+          }
+        ],
+        "prettier/prettier": "error",
+        "no-console": "warn",
+        "no-unused-vars": [
+          "warn",
+          {
+            argsIgnorePattern: "^_"
+          }
+        ]
+      }
+    },
+    {
+      files: ["*.html"],
+      extends: [
+        "plugin:@angular-eslint/template/recommended",
+        "plugin:@angular-eslint/template/accessibility"
+      ],
+      rules: {}
     }
-  ],
+  ]
 };
